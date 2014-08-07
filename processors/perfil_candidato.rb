f83a2fb8a8a34956e21bd144c96637dc36e2a0fd
Vinícius Daniel Antunes Oliveira <viniciusdaniel@gmail.com>
2014-08-07 17:35:45 -0300

    Refactory

--- processors/perfil_candidato.rb
+++ processors/perfil_candidato.rb
@@ -78,23 +78,23 @@ module Processors
       iframe = response.search('#tab-sit-procss iframe')
       data[:acompanhamento_processual_url] = clean! iframe.attr('src').text unless iframe
 
-      data.each_pair{ |k,v| @candidato[k] = v }
-      @candidato.save
+      data.each_pair{ |k,v| candidato[k] = v }
+      candidato.save
 
       download_attachments data[:url_foto], "#{id}#{fileext(data[:url_foto])}", 'perfil'
     end
 
     def parse_bens(response)
-      @candidato.bens.destroy_all
+      candidato.bens.destroy_all
       response.search('#tab-bens tbody tr').each do |bem|
         descricao = clean! bem.search('td:nth-of-type(1)').text
         valor = clean! bem.search('td:nth-of-type(2)').text
-        Eleicao::Bem.find_or_create_by(descricao: descricao, valor: valor, candidato:  @candidato)
+        Eleicao::Bem.find_or_create_by(descricao: descricao, valor: valor, candidato:  candidato)
       end
     end
 
     def parse_certidoes(response)
-      @candidato.certidoes.destroy_all
+      candidato.certidoes.destroy_all
       response.search('#tab-docs tbody tr').each do |cert|
         a = cert.search('td a')
         next if a.count.equal?(0)
@@ -102,21 +102,21 @@ module Processors
         url = clean! a.attr('href').text
         descricao = clean! a.text
 
-        Eleicao::Certidao.create(descricao: descricao, url: url, candidato:  @candidato)
+        Eleicao::Certidao.create(descricao: descricao, url: url, candidato:  candidato)
 
         download_attachments url, descricao, 'cerditoes'
       end
     end
 
     def parse_propostas(response)
-      @candidato.propostas.destroy_all
+      candidato.propostas.destroy_all
       response.search('#tab-propostas tbody tr').each do |cert|
         a = cert.search('td a')
         next if a.count.equal?(0)
 
         url = clean! a.attr('href').text
         descricao = clean! a.text
-        Eleicao::Proposta.create(descricao: descricao, url: url, candidato:  @candidato)
+        Eleicao::Proposta.create(descricao: descricao, url: url, candidato:  candidato)
 
         download_attachments url, descricao, 'propostas'
       end
@@ -126,7 +126,7 @@ module Processors
       list = response.search('#tab-corr tbody tr')
       list = response.search('#tab-titular tbody tr') unless list
 
-      @candidato.candidatos_relacionados = []
+      candidato.candidatos_relacionados = []
 
       list.each do |bem|
         id = clean! bem.search('td:nth-of-type(1)').text
@@ -138,16 +138,16 @@ module Processors
         c.url_profile = url_profile
         c.save
 
-        @candidato.candidatos_relacionados << c
+        candidato.candidatos_relacionados << c
       end
     end
 
     def parse_eleicoes(response)
-      @candidato.eleicoes.destroy_all
+      candidato.eleicoes.destroy_all
       response.search('#tab-el-anteriores tbody tr').each do |bem|
         ano = clean! bem.search('td:nth-of-type(1)').text
         url = clean! bem.search('td:nth-of-type(2) a').attr('href').text
-        Eleicao::Eleicao.create(ano: ano, url: url, candidato:  @candidato)
+        Eleicao::Eleicao.create(ano: ano, url: url, candidato:  candidato)
       end
     end
 
@@ -157,33 +157,33 @@ module Processors
 
       final_path = File.join path, filename_format(filename)
 
-      @logger.info "Download #{base_url url}"
-      @scraper.download base_url(url), final_path
+      logger.info "Download #{base_url url}"
+      scraper.download base_url(url), final_path
     end
 
     def process
-      if @candidato.nil?
+      if candidato.nil?
         logger.warn "Candidato não encontrado pelo ID #{id}"
       else
-        logger.info "Candidato: #{@candidato.id} - #{@candidato.nome_completo}"
+        logger.info "Candidato: #{candidato.id} - #{candidato.nome_completo}"
 
         begin
-          response = @scraper.get base_url(@candidato.url_profile)
+          response = scraper.get base_url(candidato.url_profile)
           persist_raw response.body if @persist_raw
 
-          parse_perfil response if @should_parse[:perfi]
-          parse_bens response if @should_parse[:bens]
-          parse_certidoes response if @should_parse[:certidoes]
-          parse_propostas response if @should_parse[:propostas]
-          parse_suplentes response if @should_parse[:suplentes]
-          parse_eleicoes response if @should_parse[:eleicoes]
+          parse_perfil response if should_parse[:perfi]
+          parse_bens response if should_parse[:bens]
+          parse_certidoes response if should_parse[:certidoes]
+          parse_propostas response if should_parse[:propostas]
+          parse_suplentes response if should_parse[:suplentes]
+          parse_eleicoes response if should_parse[:eleicoes]
 
-          @candidato.scraped_at = Time.now
-          @candidato.save
+          candidato.scraped_at = Time.now
+          candidato.save
         rescue Exception => e
-          logger.error "SCRAPER ERROR: Candidato ID :#{@candidato.id} #{e.backtrace.join("\n")}"
-          @candidato.scraped_at = nil
-          @candidato.save
+          logger.error "SCRAPER ERROR: Candidato ID :#{candidato.id} #{e.backtrace.join("\n")}"
+          candidato.scraped_at = nil
+          candidato.save
         end
       end
     end
