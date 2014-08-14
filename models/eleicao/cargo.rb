9ad1173916ebff8737dc0035ff4b5a1f7a2c6cd4
Vinícius Daniel Antunes Oliveira <viniciusdaniel@gmail.com>
2014-08-14 17:18:18 -0300

    Fix relacionamento de cargo de candidato

--- models/eleicao/cargo.rb
+++ models/eleicao/cargo.rb
@@ -7,7 +7,7 @@ module Eleicao
 
     include Mongoid::Timestamps
 
-    belongs_to :candidato, class_name: "Eleicao::Candidato"
+    has_many :candidatos, class_name: "Eleicao::Candidato"
 
     index({ id: 1 }, { unique: true, background: true })
     index({ nome: 1 }, { unique: true, background: true })
